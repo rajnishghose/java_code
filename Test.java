20dca54e6e377434cef2336f8a8c9b7adc260aa0
rajnishghose@gmail.com <rajnishghose@gmail.com>
2021-06-16 19:18:17 +0530

    added java file to validate cicd pipleline

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+	for (int i=1;i<=10;i++)
+        	{
+        	System.out.println("Hello Rajnish");
+	        System.out.println("Hello Kumar");
+        	}
+	}
+
+}
+
+
